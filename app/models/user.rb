a6a52ac46d6f8d4f2faf5ffc6c0be60d7eeeb77b
Brent Cameron <bjcameron@gmail.com>
2021-04-06 09:14:21 -0600

    mapped out the polymorphic follow association properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :follows, foreign_key: :follower_id 
-  has_many :cryptocurrencies, through: :follows
+  has_many :cryptocurrencies, through: :follows, source: :followable, source_type: 'Cryptocurrency'
+  
+  # has_many :wallets, through: :follows, source: :followable, source_type: 'Wallet'
+
 end
