9f32494ab5ff650c6942be3e6a3a795a090e7396
Brent Cameron <bjcameron@gmail.com>
2021-04-12 10:49:10 -0600

    show a list of currently followed cryptocurrencies with links on users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,28 @@ class UsersController < ApplicationController
     def index 
         @users = User.all
 
-        # require 'httpclient'
-        # require 'json'
+        # # require 'httpclient'
+        # # require 'json'
 
-        client = HTTPClient.new
+        # client = HTTPClient.new
 
-        response = client.get('https://chain.so/api/v2/get_info/DOGE')
+        # response = client.get('https://chain.so/api/v2/get_info/DOGE')
 
-        if response.status_code == 200 then
-        # everything went swimmingly
+        # if response.status_code == 200 then
+        # # everything went swimmingly
 
-        content = JSON.parse response.content
-        @content = content
+        # content = JSON.parse response.content
+        # @content = content
 
-        @name = "Name: "+content['data']['name']
-        @blocks = "Total Blocks: "+content['data']['blocks'].to_s
+        # @name = "Name: "+content['data']['name']
+        # @blocks = "Total Blocks: "+content['data']['blocks'].to_s
 
-        end
+        # end
     end
 
     def show 
         @user = User.find(params[:id])
+        @follows = @user.follows
+        @cryptocurrencies = @user.cryptocurrencies 
     end
 end
