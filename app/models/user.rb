276f5d770effc70fdc756f923b7857d620dcc611
Brent Cameron <bjcameron@gmail.com>
2021-04-12 16:34:57 -0600

    added user.wallets relationship in the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :follows, foreign_key: :follower_id 
   has_many :cryptocurrencies, through: :follows, source: :followable, source_type: 'Cryptocurrency'
   
-  # has_many :wallets, through: :follows, source: :followable, source_type: 'Wallet'
+  has_many :wallets, through: :follows, source: :followable, source_type: 'Wallet'
 
 end
