ae7ba81ba04cbd28d73ac58d6a8820d5dac97671
Brent Cameron <bjcameron@gmail.com>
2021-04-13 16:53:05 -0600

    created dropdown for selection of cryptocurrency on new wallet

--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -2,4 +2,7 @@ class Cryptocurrency < ApplicationRecord
     has_many :followers, as: :followable
 
     has_many :wallets
+    def symbol_name 
+        "#{Cryptocurrency.symbol} - name"
+    end
 end
